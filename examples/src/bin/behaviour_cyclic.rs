f7ba084c510179e03d03946cc0c1f49eec01a318
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-12-05 14:50:13 +0100

    refactor: Extract example boilerplate into declarative macro

--- examples/src/bin/behaviour_cyclic.rs
+++ examples/src/bin/behaviour_cyclic.rs
@@ -1,6 +1,10 @@
 #![cfg_attr(target_os = "none", no_std)]
 #![cfg_attr(target_os = "none", no_main)]
 
+use no_std_framework_examples::setup_example;
+
+setup_example!();
+
 use no_std_framework_core::behaviour::{CyclicBehaviour, OneShotBehaviour, SimpleBehaviourState};
 use no_std_framework_core::{Agent, Container};
 
@@ -34,34 +38,4 @@ fn example() {
             )),
     );
     container.start().unwrap();
-}
-
-#[cfg(target_os = "none")]
-mod esp_imports {
-    pub(super) use esp_backtrace as _;
-    pub(super) use esp_hal::prelude::*;
-
-    pub(super) use no_std_framework_examples::esp;
-}
-
-#[cfg(target_os = "none")]
-use esp_imports::*;
-
-#[cfg(target_os = "none")]
-#[entry]
-fn main() -> ! {
-    esp_println::logger::init_logger_from_env();
-    esp::init_heap();
-
-    example();
-
-    panic!("End of program")
-}
-
-#[cfg(not(target_os = "none"))]
-fn main() {
-    use no_std_framework_examples::local;
-    local::init_logger(log::LevelFilter::Trace);
-
-    example();
 }
--- examples/src/lib.rs
+++ examples/src/lib.rs
@@ -5,3 +5,40 @@ pub mod esp;
 
 #[cfg(not(target_os = "none"))]
 pub mod local;
+
+mod setup_example {
+    #[macro_export]
+    macro_rules! setup_example {
+        () => {
+            #[cfg(target_os = "none")]
+            mod esp_imports {
+                pub(super) use esp_backtrace as _;
+                pub(super) use esp_hal::prelude::*;
+
+                pub(super) use no_std_framework_examples::esp;
+            }
+
+            #[cfg(target_os = "none")]
+            use esp_imports::*;
+
+            #[cfg(target_os = "none")]
+            #[entry]
+            fn main() -> ! {
+                esp_println::logger::init_logger_from_env();
+                esp::init_heap();
+
+                example();
+
+                panic!("End of program");
+            }
+
+            #[cfg(not(target_os = "none"))]
+            fn main() {
+                use no_std_framework_examples::local;
+                local::init_logger(log::LevelFilter::Trace);
+
+                example();
+            }
+        };
+    }
+}
