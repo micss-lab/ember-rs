6aed5209835c9868a854c3132e4d0d823d147f6e
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-12-24 18:32:19 +0100

    example(arduino-lib): Add example for sequential behaviours

--- arduino/sequential/sequential.ino
+++ arduino/sequential/sequential.ino
@@ -1,13 +1,14 @@
 #include <Framework.h>
 
 #include <utility>
+#include <iostream>
 
 class HelloWorld: 
     public framework::behaviour::OneShotBehaviour {
   public:
     virtual void action(framework::behaviour::Context& context) const override {
       Serial.println("Hello, World!");
-      Serial.println("My friend will print 10 messages.");
+      Serial.println("My friends will print 10 messages followed by 20 of something else.");
     }
 };
 
@@ -33,13 +34,44 @@ class MessagePrinter:
     unsigned int count{10};
 };
 
+class SomethingElse:
+    public framework::behaviour::CyclicBehaviour {
+  public:
+    virtual void action(framework::behaviour::Context& context) override {
+        Serial.println("Something else...");
+        --this->count;
+    }
+    virtual bool is_finished() const override {
+      return this->count == 0;
+    }
+  private:
+    unsigned int count{20};
+};
+
+class SomethingSequential:
+    public framework::behaviour::SequentialBehaviour {
+  public:
+    SomethingSequential():
+     framework::behaviour::SequentialBehaviour(
+       SomethingSequential::initial_behaviours()
+     ) {}
+
+    static framework::behaviour::SequentialBehaviourQueue initial_behaviours() {
+      framework::behaviour::SequentialBehaviourQueue queue;
+      queue.add_behaviour(std::make_unique<HelloWorld>());
+      queue.add_behaviour(std::make_unique<MessagePrinter>());
+      queue.add_behaviour(std::make_unique<SomethingElse>());
+      return queue;
+    }
+};
+
 void setup() {
   Serial.begin(115200);
   Serial.println("Hello, ESP32-C3!");
 
   // Initialize the frameworks required resources.
   framework::initialize(framework::logging::LogLevel::Debug);
-  /* framework::__ffi::initialize_logging(5); */
+  // framework::__ffi::initialize_logging(5);
   /* framework::__ffi::initialize_allocator(); */
 
   // Create the main container instance.
@@ -47,8 +79,8 @@ void setup() {
   /* framework::__ffi::Container* container = framework::__ffi::container_new(); */
 
   framework::Agent hello_world_agent = framework::Agent("hello-world-agent");
-  hello_world_agent.add_behaviour(std::make_unique<HelloWorld>());
-  hello_world_agent.add_behaviour(std::make_unique<MessagePrinter>());
+
+  hello_world_agent.add_behaviour(std::make_unique<SomethingSequential>());
 
   container.add_agent(std::move(hello_world_agent));
 
