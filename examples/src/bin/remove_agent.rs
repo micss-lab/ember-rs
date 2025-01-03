f6ef11b376a18ce376f72ffc72490963cb7bc30e
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2025-01-03 09:08:33 +0100

    examples: Add example for removing an agent

---
+++ examples/src/bin/remove_agent.rs
@@ -0,0 +1,57 @@
+#![cfg_attr(target_os = "none", no_std)]
+#![cfg_attr(target_os = "none", no_main)]
+
+use no_std_framework_examples::setup_example;
+
+setup_example!();
+
+use no_std_framework_core::behaviour::{BehaviourId, Context, CyclicBehaviour, OneShotBehaviour};
+use no_std_framework_core::{Agent, Container};
+
+const MESSAGE_AMOUNT: usize = 10;
+
+struct InformationPrinter;
+
+impl OneShotBehaviour for InformationPrinter {
+    type Message = ();
+
+    fn action(&self, _: &mut Context<Self::Message>) {
+        log::info!("This agent has one behaviour.");
+        log::info!("It will seemingly print for an infinite amount of time, though it removes the agent after {MESSAGE_AMOUNT} of messages");
+    }
+}
+
+struct MessagePrinter {
+    count: usize,
+}
+
+impl MessagePrinter {
+    fn new(count: usize) -> Self {
+        Self { count }
+    }
+}
+
+impl CyclicBehaviour for MessagePrinter {
+    type Message = ();
+
+    fn action(&mut self, ctx: &mut Context<Self::Message>) {
+        log::info!("Hello there!");
+        self.count -= 1;
+        if self.count == 0 {
+            ctx.remove_agent();
+        }
+    }
+
+    fn is_finished(&self) -> bool {
+        false
+    }
+}
+
+fn example() {
+    let container = Container::default().with_agent(
+        Agent::new("messaging-agent")
+            .with_behaviour(InformationPrinter)
+            .with_behaviour(MessagePrinter::new(MESSAGE_AMOUNT)),
+    );
+    container.start().unwrap();
+}
