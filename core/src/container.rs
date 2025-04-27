5ed88a5d6ad00de364179787c516a26deb974a7b
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2025-04-27 17:13:35 +0200

    refactor(core)!: Remove idea of sub-containers
    This feature is not inherent to the FIPA spec. Supporting it in a
    synchronous application would be a nightmare.

--- core/src/container.rs
+++ core/src/container.rs
@@ -1,89 +1,24 @@
 use alloc::borrow::Cow;
 use alloc::boxed::Box;
 use alloc::collections::VecDeque;
+use alloc::vec::Vec;
 
+use crate::acl::message::Message;
 use crate::adt::Adt;
 use crate::agent::{Agent, Aid, AmsAgent};
 use crate::context::ContainerContext;
-use crate::util::sync::AtomicBool;
-
-use self::kind::{ContainerKind, Main};
 
 mod mts;
 
-static MAIN_CONTAINER_CREATED: AtomicBool = AtomicBool::new(false);
-
-pub struct Container<K = Main> {
+pub struct Container {
     /// Agents managed by this container.
     agents: VecDeque<Box<dyn AgentLike>>,
-    kind: K,
+    /// Ams agent managing this cotainers.
+    ams: AmsAgent,
+    /// Register of agents running on this platform.
+    ladt: Adt,
 }
 
-mod kind {
-    use alloc::boxed::Box;
-    use alloc::collections::VecDeque;
-    use alloc::vec::Vec;
-
-    use crate::acl::message::Message;
-    use crate::adt::Adt;
-    use crate::agent::{Aid, AmsAgent};
-    use crate::context::ContainerContext;
-
-    use super::{AgentLike, Container};
-
-    /// The container is a main container and can contain sub-containers.
-    pub struct Main {
-        /// Ams agent managing this and all sub-cotainers.
-        pub(super) ams: AmsAgent,
-        /// Sub-containers managed by this main container.
-        pub(super) containers: VecDeque<Container<Sub>>,
-        /// Register of agents running on this platform.
-        pub(super) ladt: Adt,
-    }
-    /// The container is a sub-container, thus cannot contain more sub-containers.
-    pub(super) struct Sub;
-
-    pub trait ContainerKind {
-        fn poll_associated_agents(&mut self) -> Result<(), Box<dyn core::error::Error>> {
-            Ok(())
-        }
-
-        fn poll_sub_containers(&mut self) -> Result<(), Box<dyn core::error::Error>> {
-            Ok(())
-        }
-    }
-
-    impl ContainerKind for Main {
-        fn poll_associated_agents(&mut self) -> Result<(), Box<dyn core::error::Error>> {
-            let mut context =
-                ContainerContext::new(self.messages_for_agent(&Aid::ams()).unwrap_or_default());
-            self.ams.update(&mut context);
-            self.ams.perform_platform_actions(&mut self.ladt);
-            Ok(())
-        }
-
-        fn poll_sub_containers(&mut self) -> Result<(), Box<dyn core::error::Error>> {
-            // TODO: Implement sub-containers.
-            Ok(())
-        }
-    }
-
-    impl Main {
-        pub(super) fn messages_for_agent(&mut self, aid: &Aid) -> Option<Vec<Message>> {
-            use crate::acl::message::MessageKind;
-            Some(
-                core::mem::take(&mut self.ladt.get_mut(aid)?.inbox)
-                    .into_iter()
-                    .map(|m| match m.message {
-                        MessageKind::Structured(m) => m,
-                    })
-                    .collect(),
-            )
-        }
-    }
-
-    impl ContainerKind for Sub {}
-}
 pub trait AgentLike: 'static {
     fn update(&mut self, context: &mut ContainerContext) -> bool;
 
@@ -92,18 +27,7 @@ pub trait AgentLike: 'static {
     fn get_aid(&self) -> Aid;
 }
 
-impl Container<Main> {
-    pub fn with_agent<E: 'static>(mut self, agent: Agent<E>) -> Self {
-        self.add_agent(agent);
-        self
-    }
-
-    pub fn add_agent<E: 'static>(&mut self, agent: Agent<E>) {
-        self.agents.push_back(Box::new(agent));
-    }
-}
-
-impl Container<Main> {
+impl Container {
     pub fn start(mut self) -> Result<(), Box<dyn core::error::Error>> {
         loop {
             let should_stop = self.poll()?;
@@ -113,16 +37,23 @@ impl Container<Main> {
         }
     }
 
+    fn poll_associated_agents(&mut self) -> Result<(), Box<dyn core::error::Error>> {
+        let mut context =
+            ContainerContext::new(self.messages_for_agent(&Aid::ams()).unwrap_or_default());
+        self.ams.update(&mut context);
+        self.ams.perform_platform_actions(&mut self.ladt);
+        Ok(())
+    }
+
     pub fn poll(&mut self) -> Result<bool, Box<dyn core::error::Error>> {
         // Iterate over all agents once, only rescheduling agents that are not removed.
         let mut amount = self.agents.len();
 
         while let Some(mut agent) = self.agents.pop_front() {
-            self.kind.poll_associated_agents()?;
+            self.poll_associated_agents()?;
 
             let mut context = ContainerContext::new(
-                self.kind
-                    .messages_for_agent(&agent.get_aid())
+                self.messages_for_agent(&agent.get_aid())
                     .unwrap_or_default(),
             );
 
@@ -130,7 +61,7 @@ impl Container<Main> {
 
             // Handle all messages the agent wants to send.
             for message in context.message_outbox.into_iter() {
-                mts::send_message(message, &mut self.kind.ladt);
+                mts::send_message(message, &mut self.ladt);
             }
 
             if context.should_stop {
@@ -146,30 +77,40 @@ impl Container<Main> {
                 break;
             }
         }
-        self.kind.poll_sub_containers()?;
 
         Ok(false)
+    }
+
+    fn messages_for_agent(&mut self, aid: &Aid) -> Option<Vec<Message>> {
+        use crate::acl::message::MessageKind;
+        Some(
+            core::mem::take(&mut self.ladt.get_mut(aid)?.inbox)
+                .into_iter()
+                .map(|m| match m.message {
+                    MessageKind::Structured(m) => m,
+                })
+                .collect(),
+        )
+    }
+
+    pub fn with_agent<E: 'static>(mut self, agent: Agent<E>) -> Self {
+        self.add_agent(agent);
+        self
+    }
+
+    pub fn add_agent<E: 'static>(&mut self, agent: Agent<E>) {
+        self.agents.push_back(Box::new(agent));
     }
 }
 
 impl Default for Container {
     fn default() -> Self {
-        check_and_set_created();
         let ams = AmsAgent::new();
         let ladt = Adt::new(&ams);
         Self {
             agents: VecDeque::default(),
-            kind: Main {
-                ams,
-                containers: VecDeque::default(),
-                ladt,
-            },
+            ams,
+            ladt,
         }
-    }
-}
-
-fn check_and_set_created() {
-    if MAIN_CONTAINER_CREATED.compare_and_swap(false, true) {
-        panic!("Can only create a single instance of the main container.");
     }
 }
