2ef6af13053faab0fe8408c683e525d929868496
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2025-06-07 14:56:28 +0200

    arch: Switch from soc support of esp32c3 to esp32
    This comes with a lot of changes especially because of the switch from
    RiscV to XTensa as this is a tier 3 rust target.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,6 @@
     devenv.url = "github:cachix/devenv";
 
     rust-overlay.url = "github:oxalica/rust-overlay";
-    crane.url = "github:ipetkov/crane";
   };
 
   outputs = {
@@ -16,7 +15,6 @@
     flake-utils,
     devenv,
     rust-overlay,
-    crane,
     ...
   } @ inputs:
     flake-utils.lib.eachDefaultSystem (
@@ -30,157 +28,14 @@
         };
 
         rustToolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        craneLib = (crane.mkLib pkgs).overrideToolchain rustToolchain;
-
-        sources = {
-          cargo = craneLib.cleanCargoSource ./.;
-        };
-
-        metadata = {
-          cargo = {
-            pname = "no-std-framework-rs";
-            version = "0.0.0";
-          };
-        };
-
-        commonArgs = {
-          common = {
-            strictDeps = true;
-          };
-
-          cargo =
-            commonArgs.common
-            // metadata.cargo
-            // {
-              src = sources.cargo;
-
-              # Build-time dependencies.
-              nativeBuildInputs = [];
-              # Linkable build dependencies.
-              buildInputs = [];
-
-              cargoVendorDir = craneLib.vendorMultipleCargoDeps {
-                inherit (craneLib.findCargoFiles sources.cargo) cargoConfigs;
-                cargoLockList = [
-                  ./Cargo.lock
-                  # ./core/Cargo.lock
-                  # ./examples/Cargo.lock
-                  # ./tests/Cargo.lock
-                  # ./bindings/Cargo.lock
-                ];
-              };
-
-              CARGO_BUILD_TARGET = "riscv32imc-unknown-none-elf";
-            };
-        };
-
-        artifacts = {
-          cargo = craneLib.buildDepsOnly (commonArgs.cargo
-            // {
-              cargoCheckExtraArgs = "";
-              cargoTestCommand = ''
-                true
-              '';
-            });
-        };
-
-        buildArgs = {
-          cargo =
-            commonArgs.cargo
-            // {
-              cargoArtifacts = artifacts.cargo;
-              # Do not run tests during build process.
-              doCheck = false;
-            };
-        };
-
-        builds = {
-          cargo = craneLib.buildPackage buildArgs.cargo;
-        };
-      in rec {
+      in {
         formatter = pkgs.nixpkgs-fmt;
-
-        checks = {
-          # cargo-build = builds.cargo;
-          #
-          # cargo-clippy = craneLib.cargoClippy (commonArgs.cargo
-          #   // {
-          #     cargoArtifacts = artifacts.cargo;
-          #     cargoClippyExtraArgs = "-- --deny warnings";
-          #   });
-          #
-          # cargo-doc = craneLib.cargoDoc (commonArgs.cargo
-          #   // {
-          #     cargoArtifacts = artifacts.cargo;
-          #   });
-          #
-          # cargo-fmt = craneLib.cargoFmt (metadata.cargo
-          #   // {
-          #     src = sources.cargo;
-          #   });
-          #
-          # cargo-toml-fmt = craneLib.taploFmt (metadata.cargo
-          #   // {
-          #     src = pkgs.lib.sources.sourceFilesBySuffices sources.cargo [".toml"];
-          #   });
-          #
-          # cargo-test = craneLib.cargoNextest (commonArgs.cargo
-          #   // {
-          #     cargoArtifacts = artifacts.cargo;
-          #   });
-          #
-          # cargo-hack = craneLib.mkCargoDerivation (commonArgs.cargo
-          #   // {
-          #     cargoArtifacts = artifacts.cargo;
-          #
-          #     pnameSuffix = "-hack";
-          #
-          #     buildPhaseCargoCommand = ''
-          #       cargo hack check --workspace --locked
-          #     '';
-          #
-          #     nativeBuildInputs =
-          #       commonArgs.cargo.nativeBuildInputs
-          #       ++ [
-          #         pkgs.cargo-hack
-          #       ];
-          #   });
-          #
-          # cargo-machete = craneLib.mkCargoDerivation (commonArgs.cargo
-          #   // {
-          #     cargoArtifacts = artifacts.cargo;
-          #
-          #     pnameSuffix = "-machete";
-          #
-          #     buildPhaseCargoCommand = ''
-          #       cargo machete --with-metadata
-          #     '';
-          #
-          #     nativeBuildInputs =
-          #       commonArgs.cargo.nativeBuildInputs
-          #       ++ [
-          #         pkgs.cargo-machete
-          #       ];
-          #   });
-        };
 
         packages = {
           devenv-up = self.devShells.${system}.default.config.procfileScript;
         };
 
         devShells.default = devenv.lib.mkShell (let
-          lib = nixpkgs.lib;
-
-          # Inherit inputs from flake checks (inputsFrom is not available).
-          flakeChecksInputs = let
-            from = builtins.attrValues checks ++ builtins.attrValues builds;
-            extractInputs = name: (lib.subtractLists from (lib.flatten (lib.catAttrs name from)));
-          in
-            extractInputs "buildInputs"
-            ++ extractInputs "nativeBuildInputs"
-            ++ extractInputs "propagatedBuildInputs"
-            ++ extractInputs "propagatedNativeBuildInputs";
-
           devRustToolchain = rustToolchain.override {
             extensions = ["rust-analyzer" "rust-src"];
           };
@@ -188,36 +43,33 @@
           inherit inputs pkgs;
           modules = [
             ({...}: {
-              name = metadata.cargo.pname;
+              packages = [
+                devRustToolchain
+                pkgs.rustup
+                pkgs.espup
+                pkgs.bacon
 
-              cachix = {
-                enable = true;
-                pull = ["exellentcoin26-no-std-framework-rs"];
-              };
+                pkgs.arduino-cli
+                pkgs.arduino
+                (pkgs.python3.withPackages (python-pkgs: [
+                  python-pkgs.pyserial
+                ]))
 
-              packages =
-                [
-                  devRustToolchain
-                  pkgs.espup
-                  pkgs.bacon
-
-                  pkgs.arduino-cli
-                  pkgs.arduino
-                  (pkgs.python3.withPackages (python-pkgs: [
-                    python-pkgs.pyserial
-                  ]))
-
-                  pkgs.just
-                  pkgs.jq
-                ]
-                ++ flakeChecksInputs;
+                pkgs.just
+                pkgs.jq
+              ];
 
               env = {
                 RUST_SRC_PATH = "${devRustToolchain}/lib/rustlib/src/rust/library";
+                LD_LIBRARY_PATH = "$LD_LIBRARY_PATH:${pkgs.lib.makeLibraryPath [
+                  pkgs.stdenv.cc.cc.lib
+                  pkgs.zlib
+                  pkgs.libxml2
+                ]}";
               };
 
               enterShell = ''
-                export PATH="$PATH:${self}/bin";
+                export PATH="$HOME/.rustup/toolchains/esp/bin:$PATH"
               '';
             })
           ];
