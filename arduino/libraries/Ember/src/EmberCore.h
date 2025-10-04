022bd60c7d3a0103ed20797c84962fc6e1567bf5
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2025-10-04 21:53:17 +0200

    fix(bindings): Check in latest version of library

--- arduino/libraries/Ember/src/EmberCore.h
+++ arduino/libraries/Ember/src/EmberCore.h
@@ -1,5 +1,5 @@
-#ifndef FRAMEWORK_CORE_H
-#define FRAMEWORK_CORE_H
+#ifndef EMBER_CORE_H
+#define EMBER_CORE_H
 
 #include "inttypes.h"
 
@@ -48,24 +48,6 @@ AgentState *agent_state_new(void *agent_state);
 
 void agent_state_free(AgentState *agent_state);
 
-/**
- * Creates a new container instance.
- *
- * # Safety
- *
- * The ownership of the instance is transferred to the caller. Make sure to free the memory
- * with the accompanying [`container_free`].
- */
-Container *container_new();
-
-void container_free(Container *container);
-
-void container_add_agent(Container *container, Agent<AgentState, Event> *agent);
-
-int32_t container_start(Container *container);
-
-ContainerPollResult container_poll(Container *container);
-
 Agent<AgentState, Event> *agent_new(const char *name, AgentState *agent_state);
 
 void agent_free(Agent<AgentState, Event> *agent);
@@ -78,14 +60,6 @@ void agent_add_behaviour_ticker(Agent<AgentState, Event> *agent, TickerBehaviour
 
 void agent_add_behaviour_sequential(Agent<AgentState, Event> *agent,
                                     SequentialBehaviour *sequential);
-
-void context_emit_event(Context<Event> *context, Event *event);
-
-void context_stop_container(Context<Event> *context);
-
-void context_remove_agent(Context<Event> *context);
-
-void context_block_behaviour(Context<Event> *context);
 
 OneShotBehaviour *behaviour_oneshot_new(void *inner, void (*action)(void*,
                                                                     Context<Event>*,
@@ -108,15 +82,16 @@ void behaviour_ticker_free(TickerBehaviour *ticker);
 
 BehaviourVec *behaviour_vec_new();
 
-void behaviour_vec_add_behaviour_oneshot(BehaviourVec *queue, OneShotBehaviour *oneshot);
+void behaviour_vec_add_behaviour_oneshot(BehaviourVec *behaviour_vec, OneShotBehaviour *oneshot);
 
-void behaviour_vec_add_behaviour_cyclic(BehaviourVec *queue, CyclicBehaviour *cyclic);
+void behaviour_vec_add_behaviour_cyclic(BehaviourVec *behaviour_vec, CyclicBehaviour *cyclic);
 
-void behaviour_vec_add_behaviour_ticker(BehaviourVec *queue, TickerBehaviour *ticker);
+void behaviour_vec_add_behaviour_ticker(BehaviourVec *behaviour_vec, TickerBehaviour *ticker);
 
-void behaviour_vec_add_behaviour_sequential(BehaviourVec *queue, SequentialBehaviour *sequential);
+void behaviour_vec_add_behaviour_sequential(BehaviourVec *behaviour_vec,
+                                            SequentialBehaviour *sequential);
 
-void behaviour_vec_free(BehaviourVec *queue);
+void behaviour_vec_free(BehaviourVec *behaviour_vec);
 
 SequentialBehaviour *behaviour_sequential_new(void *inner,
                                               BehaviourVec *initial_behaviours,
@@ -126,6 +101,32 @@ SequentialBehaviour *behaviour_sequential_new(void *inner,
                                                                          AgentState*));
 
 void behaviour_sequential_free(SequentialBehaviour *sequential);
+
+/**
+ * Creates a new container instance.
+ *
+ * # Safety
+ *
+ * The ownership of the instance is transferred to the caller. Make sure to free the memory
+ * with the accompanying [`container_free`].
+ */
+Container *container_new();
+
+void container_free(Container *container);
+
+void container_add_agent(Container *container, Agent<AgentState, Event> *agent);
+
+int32_t container_start(Container *container);
+
+ContainerPollResult container_poll(Container *container);
+
+void context_emit_event(Context<Event> *context, Event *event);
+
+void context_stop_container(Context<Event> *context);
+
+void context_remove_agent(Context<Event> *context);
+
+void context_block_behaviour(Context<Event> *context);
 
 /**
  * Initialize the libraries global logger.
@@ -139,4 +140,4 @@ void initialize_logging(char level);
 
 }  // namespace ember::__ffi
 
-#endif  // FRAMEWORK_CORE_H
+#endif  // EMBER_CORE_H
