f0e7b2ac2ca6ab675cea5f4e92873190ebb390df
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2026-06-29 22:04:42 +0200

    chore(ember-bdi): Remove variable typing
    This was planned as a feature to only allow unification when variable
    types match. At the moment, I do not think this is valuable enough to
    implement.

--- ember-bdi/src/variable.rs
+++ ember-bdi/src/variable.rs
@@ -1,5 +1,3 @@
-use core::marker::PhantomData;
-
 use ember_util::sync::AtomicU32;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
@@ -9,48 +7,14 @@ type VarID = u32;
 
 static NEXT_VARIABLE_ID: AtomicU32 = AtomicU32::new(0);
 
-#[derive(Debug, Clone)]
-pub struct Variable<T = ()> {
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Variable {
     pub(crate) id: VariableId,
-    /// Type of the value storable by the variable. During unification,if the type of the
-    /// variable does not match, it will reject the unification attempt.
-    type_: (VariableType, PhantomData<T>),
 }
 
-impl<T> Variable<T> {
-    #[allow(clippy::new_without_default)]
+impl Variable {
     pub fn new() -> Self {
         let id = NEXT_VARIABLE_ID.get_increment();
-        Self {
-            id: VariableId(id),
-            type_: (VariableType::Any, PhantomData),
-        }
+        Self { id: VariableId(id) }
     }
-}
-
-impl PartialEq for Variable<()> {
-    fn eq(&self, other: &Self) -> bool {
-        self.id == other.id && self.type_.0 == other.type_.0
-    }
-}
-
-impl Eq for Variable<()> {}
-
-impl PartialOrd for Variable<()> {
-    fn partial_cmp(&self, other: &Self) -> Option<core::cmp::Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Variable<()> {
-    fn cmp(&self, other: &Self) -> core::cmp::Ordering {
-        self.id.cmp(&other.id)
-    }
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum VariableType {
-    Any,
-    String,
-    Number,
 }
