16df1c091fab9e3ba72c984a4b9b0fd98c92cee0
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2026-09-05 12:36:39 +0200

    refactor(ember-acc): Rework the http acc to be much more readable

--- ember-acc/src/http.rs
+++ ember-acc/src/http.rs
@@ -4,16 +4,15 @@ use std::time::Duration;
 use std::vec::Vec;
 
 use bytes::{BufMut, Bytes, BytesMut};
-use ember_core::environment::Environment;
 use multipart::server::{Multipart, ReadEntry, ReadEntryResult};
-use serde::ser::SerializeStruct;
 use tiny_http::Server;
 
 use ember_core::agent::aid::Aid;
+use ember_core::environment::Environment;
 use ember_core::message::{AclRepresentation, Message, MessageEnvelope, Payload};
 use ember_core::message::{MessageEnvelopes, TransportMessage, repr};
 
-use super::Acc;
+use super::{Acc, SendError};
 
 pub(super) struct HttpChannel {
     server: Server,
@@ -32,48 +31,78 @@ impl Acc for HttpChannel {
         &mut self,
         address: &Aid,
         message: TransportMessage,
-        // TODO: Make use of these callbacks.
-        _callbacks: super::SendCallbacks,
-        _environment: &mut Environment,
-    ) -> Result<(), super::SendError> {
-        use rand::RngCore;
-        let mut boundary = [0u8; 16];
-        rand::rng().fill_bytes(&mut boundary);
+        mut callbacks: super::SendCallbacks,
+        environment: &mut Environment,
+    ) -> Result<(), SendError> {
+        let envelope = HttpEnvelopeSer {
+            to: &message.envelopes.base.to,
+            from: &message.envelopes.base.from,
+        };
+        let envelope_bytes =
+            serde_bencode::to_bytes(&envelope).expect("failed to serialize message to bencode");
 
-        let response = match ureq::post(aid_to_url(address))
+        let content = match &message.payload {
+            Payload::AclMessage(m) => m.to_string(),
+            Payload::Bytes(_) => unimplemented!(),
+        };
+
+        let mut body = MultipartBody::new();
+        body.part("application/custom.mts.env.rep.bencode", &envelope_bytes);
+        // TODO: Match the correct payload encoding here.
+        body.part(
+            "application/fipa.acl.rep.string.std; charset=US-ASCII",
+            content.as_bytes(),
+        );
+        let content_type = format!(
+            "multipart/mixed; boundary=\"{}\"; charset=\"ascii\"",
+            body.boundary()
+        );
+        let body = body.finish();
+
+        let result = ureq::post(aid_to_url(address))
             .version(ureq::http::Version::HTTP_11)
-            .content_type(format!(
-                "multipart/mixed; boundary=\"{}\"; charset=\"ascii\"",
-                hex::encode(boundary)
-            ))
+            .content_type(content_type)
             .header("HOST", self.server.server_addr().to_string())
             .header("Cache-Control", "no-cache")
             .header("MIME-Version", "1.0")
             .config()
             .timeout_recv_response(Some(Duration::from_millis(50)))
             .build()
-            .send(encode_message(message, &boundary).as_ref())
-        {
-            Ok(res) => res,
+            .send(body.as_ref());
+
+        let outcome = match result {
+            Ok(response) => {
+                log::debug!("Received response: {:?}", response);
+                Ok(())
+            }
             Err(ureq::Error::Timeout(ureq::Timeout::RecvResponse)) => {
-                // TODO: Handle this.
-                log::warn!("Remote acc did not respond");
-                return Err(super::SendError::Generic("".into()));
+                Err(SendError::Generic("remote acc did not respond".into()))
             }
-            Err(e) => {
-                log::error!("failed to send message: {e}");
-                return Err(super::SendError::Generic("".into()));
-            }
+            Err(e) => Err(SendError::Generic(
+                format!("failed to send message: {e}").into(),
+            )),
         };
 
-        log::debug!("Received response: {:?}", response);
+        match outcome.is_ok() {
+            true => {
+                if let Some(on_success) = callbacks.on_success.take() {
+                    on_success(environment);
+                }
+            }
+            false => {
+                if let Some(on_failure) = callbacks.on_failure.take() {
+                    on_failure(environment);
+                }
+            }
+        }
+        if let Some(on_complete) = callbacks.on_complete.take() {
+            on_complete(environment);
+        }
 
-        Ok(())
+        outcome
     }
 
     fn receive(&mut self, _environment: &mut super::Environment) -> Option<TransportMessage> {
-        use std::io::Read;
-
         let mut req = self.server.try_recv().expect("receiving message failed")?;
         log::debug!("Request received: {:?}", req);
 
@@ -82,19 +111,10 @@ impl Acc for HttpChannel {
             return None;
         };
 
-        let ReadEntryResult::Entry(mut envelope) = req.read_entry_mut() else {
-            log::error!("Error extracting message envelope from multipart request");
-            return None;
-        };
-
         let mut buf = Vec::with_capacity(128);
-        let len = envelope
-            .data
-            .read_to_end(&mut buf)
-            .expect("failed to read envelope data");
-        log::trace!("Read envelope of length {} bytes", len);
-        log::debug!("Envelope: `{}`", bstr::BString::from(buf.trim_ascii()));
 
+        read_part(&mut req, &mut buf, "message envelope")?;
+        log::debug!("Envelope: `{}`", bstr::BString::from(buf.trim_ascii()));
         let envelope = match serde_bencode::from_bytes::<HttpEnvelopeDe>(&buf) {
             Ok(envelope) => envelope,
             Err(e) => {
@@ -103,19 +123,8 @@ impl Acc for HttpChannel {
             }
         };
 
-        let ReadEntryResult::Entry(mut message) = req.read_entry_mut() else {
-            log::error!("Error extracting message from multipart request");
-            return None;
-        };
-
-        buf.clear();
-        let len = message
-            .data
-            .read_to_end(&mut buf)
-            .expect("failed to read acl message data");
-        log::trace!("Read acl message of length {} bytes", len);
+        read_part(&mut req, &mut buf, "message")?;
         log::debug!("Acl message: `{}`", bstr::BString::from(buf.trim_ascii()));
-
         let message = match repr::payload::string::decode(buf.as_slice()) {
             Ok(message) => message,
             Err(_e) => {
@@ -128,105 +137,33 @@ impl Acc for HttpChannel {
     }
 }
 
-struct HttpEnvelopeSer<'a>(&'a MessageEnvelope);
+fn read_part<M: ReadEntry>(multipart: &mut M, buf: &mut Vec<u8>, label: &str) -> Option<()> {
+    use std::io::Read;
+
+    let ReadEntryResult::Entry(mut entry) = multipart.read_entry_mut() else {
+        log::error!("Error extracting {label} from multipart request");
+        return None;
+    };
+
+    buf.clear();
+    let len = entry
+        .data
+        .read_to_end(buf)
+        .expect("failed to read multipart entry data");
+    log::trace!("Read {label} of length {len} bytes");
+    Some(())
+}
+
+#[derive(serde::Serialize)]
+struct HttpEnvelopeSer<'a> {
+    to: &'a [Aid],
+    from: &'a Aid,
+}
+
+#[derive(serde::Deserialize)]
 struct HttpEnvelopeDe {
     to: Vec<Aid>,
     from: Aid,
-}
-
-impl serde::Serialize for HttpEnvelopeSer<'_> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        // TODO: Serialize other parameters as well.
-        let mut envelope = serializer.serialize_struct("envelope", 2)?;
-        envelope.serialize_field("to", &self.0.to)?;
-        envelope.serialize_field("from", &self.0.from)?;
-        envelope.end()
-    }
-}
-
-impl<'de> serde::Deserialize<'de> for HttpEnvelopeDe {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: serde::Deserializer<'de>,
-    {
-        enum Field {
-            To,
-            From,
-        }
-
-        impl<'de> serde::Deserialize<'de> for Field {
-            fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-            where
-                D: serde::Deserializer<'de>,
-            {
-                struct FieldVisitor;
-
-                impl<'de> serde::de::Visitor<'de> for FieldVisitor {
-                    type Value = Field;
-
-                    fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                        formatter.write_str("`to` or `from`")
-                    }
-
-                    fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-                    where
-                        E: serde::de::Error,
-                    {
-                        Ok(match v {
-                            "to" => Field::To,
-                            "from" => Field::From,
-                            _ => return Err(serde::de::Error::unknown_field(v, FIELDS)),
-                        })
-                    }
-                }
-
-                deserializer.deserialize_identifier(FieldVisitor)
-            }
-        }
-
-        struct HttpEnvelopeDeVisitor;
-
-        impl<'de> serde::de::Visitor<'de> for HttpEnvelopeDeVisitor {
-            type Value = HttpEnvelopeDe;
-
-            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
-                formatter.write_str("struct envelope")
-            }
-
-            fn visit_map<A>(self, mut map: A) -> Result<Self::Value, A::Error>
-            where
-                A: serde::de::MapAccess<'de>,
-            {
-                let mut to = None;
-                let mut from = None;
-                while let Some(key) = map.next_key()? {
-                    match key {
-                        Field::To => {
-                            if to.is_some() {
-                                return Err(serde::de::Error::duplicate_field("to"));
-                            }
-                            to = Some(map.next_value()?);
-                        }
-                        Field::From => {
-                            if from.is_some() {
-                                return Err(serde::de::Error::duplicate_field("from"));
-                            }
-                            from = Some(map.next_value()?);
-                        }
-                    }
-                }
-                let to = to.ok_or_else(|| serde::de::Error::missing_field("to"))?;
-                let from = from.ok_or_else(|| serde::de::Error::missing_field("from"))?;
-                Ok(HttpEnvelopeDe { to, from })
-            }
-        }
-
-        const FIELDS: &[&str] = &["to", "from"];
-        deserializer.deserialize_struct("envelope", FIELDS, HttpEnvelopeDeVisitor)
-    }
 }
 
 impl HttpEnvelopeDe {
@@ -249,62 +186,47 @@ impl HttpEnvelopeDe {
     }
 }
 
-fn encode_message(message: TransportMessage, boundary: &[u8; 16]) -> Bytes {
-    let boundary = hex::encode(boundary);
-    let mut body = BytesMut::new();
+struct MultipartBody {
+    body: BytesMut,
+    boundary: String,
+}
 
-    let envelope = message.envelopes.base;
-    let message = message.payload;
+impl MultipartBody {
+    fn new() -> Self {
+        use rand::RngCore;
+        let mut boundary = [0u8; 16];
+        rand::rng().fill_bytes(&mut boundary);
 
-    // Preamble.
-    body.put_slice(b"This is not part of the MIME multipart encoded message.");
-    body.put_slice(b"\r\n");
-
-    // Message Envelope boundary.
-    body.put_slice(b"--");
-    body.put_slice(boundary.as_bytes());
-    body.put_slice(b"\r\n");
-
-    // Message Envelope headers.
-    body.put_slice(b"Content-Type: application/custom.mts.env.rep.bencode");
-    body.put_slice(b"\r\n");
-    body.put_slice(b"\r\n");
-
-    // Message Envelope.
-    body.put(
-        &*serde_bencode::to_bytes(&HttpEnvelopeSer(&envelope))
-            .expect("failed to serialize message to bencode"),
-    );
-    body.put_slice(b"\r\n");
-    body.put_slice(b"\r\n");
-
-    // Message Body boundary.
-    body.put_slice(b"--");
-    body.put_slice(boundary.as_bytes());
-    body.put_slice(b"\r\n");
-
-    // Message Body headers.
-    // TODO: Match the correct payload encoding here.
-    body.put_slice(b"Content-Type: application/fipa.acl.rep.string.std; charset=US-ASCII");
-    body.put_slice(b"\r\n");
-    body.put_slice(b"\r\n");
-
-    // Message Body.
-    match message {
-        Payload::AclMessage(m) => body.put_slice(m.to_string().as_bytes()),
-        Payload::Bytes(_) => unimplemented!(),
+        let mut body = BytesMut::new();
+        body.put_slice(b"This is not part of the MIME multipart encoded message.\r\n");
+        Self {
+            body,
+            boundary: hex::encode(boundary),
+        }
     }
-    body.put_slice(b"\r\n");
-    body.put_slice(b"\r\n");
 
-    // End boundary.
-    body.put_slice(b"--");
-    body.put_slice(boundary.as_bytes());
-    body.put_slice(b"--");
-    body.put_slice(b"\r\n");
-    body.put_slice(b"\r\n");
+    fn boundary(&self) -> &str {
+        &self.boundary
+    }
 
-    body.freeze()
+    fn part(&mut self, content_type: &str, data: &[u8]) -> &mut Self {
+        self.body.put_slice(b"--");
+        self.body.put_slice(self.boundary.as_bytes());
+        self.body.put_slice(b"\r\n");
+        self.body.put_slice(b"Content-Type: ");
+        self.body.put_slice(content_type.as_bytes());
+        self.body.put_slice(b"\r\n\r\n");
+        self.body.put_slice(data);
+        self.body.put_slice(b"\r\n\r\n");
+        self
+    }
+
+    fn finish(mut self) -> Bytes {
+        self.body.put_slice(b"--");
+        self.body.put_slice(self.boundary.as_bytes());
+        self.body.put_slice(b"--\r\n\r\n");
+        self.body.freeze()
+    }
 }
 
 fn aid_to_url(aid: &Aid) -> String {
@@ -314,4 +236,25 @@ fn aid_to_url(aid: &Aid) -> String {
         Public(h) => h,
     };
     format!("http://{}/acc", host)
+}
+
+#[cfg(test)]
+mod tests {
+    use alloc::vec;
+
+    use super::*;
+
+    #[test]
+    fn envelope_bencode_round_trip() {
+        let from = Aid::local("alice");
+        let to = vec![Aid::local("bob")];
+        let ser = HttpEnvelopeSer {
+            to: &to,
+            from: &from,
+        };
+        let bytes = serde_bencode::to_bytes(&ser).expect("serialize");
+        let de: HttpEnvelopeDe = serde_bencode::from_bytes(&bytes).expect("deserialize");
+        assert_eq!(de.to, to);
+        assert_eq!(de.from, from);
+    }
 }
